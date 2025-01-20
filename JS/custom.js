7426bd526741207dcb99ba6a00be1935bbf92307
LilianPOLY <poly.lilian.28220@gmail.com>
2025-01-20 11:51:11 +0100

    Finalisation du Portfolio

--- JS/custom.js
+++ JS/custom.js
@@ -1,11 +1,27 @@
+AOS.init();
+
 // Menu burger
 
 const menuHamburger = document.querySelector(".burger_menu")
-const navLinks = document.querySelector(".nav-liens")
+const navLinks = document.querySelector(".nav-links")
  
         menuHamburger.addEventListener('click',()=>{
         navLinks.classList.toggle('mobile-menu')
         })
+
+const navMenu = document.getElementById('nav-links');
+const navItems = document.querySelectorAll('.nav-item');
+
+for(var i = 0; i < navItems.length; i++){
+    navItems[i].addEventListener("click", function(){
+        var current = document.querySelector(".active")
+
+        if(current){
+          current.classList.remove('active')
+        }
+        this.classList.add('active')
+    })
+}
 
 // Compétences - Barre de progression 
 
